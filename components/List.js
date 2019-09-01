7837f4be2c902ecf09709d2a66dc54295aa4fbf9
svidhaji <svidhaji@gmail.com>
2019-09-01 22:01:11 +0300

    task B small errors persist

--- components/List.js
+++ components/List.js
@@ -7,18 +7,33 @@ import ListItem from './ListItem';
 import {MediaContext} from '../contexts/MediaContext';
 
 const dataURL = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+const mediaURL = 'http://http://media.mw.metropolia.fi/wbma/uploads/';
 const List = (props) => {
   const [media, setMedia] = useContext(MediaContext);
   console.log(media);
 
   const getMedia = () => {
     fetch(dataURL)
-        .then((response) => {
+        .then(response => {
           return response.json();
         })
-        .then((responseJson) => {
-          console.log('fetched from server', responseJson);
-          setMedia(responseJson);
+        .then(result => {
+          console.log('fetced from server: ', result);
+          for (let i = 0; i < 5; i++) {
+            let id = result[i].file_id;
+            fetch(mediaURL + id)
+                .then(response => {
+                  return response.json();
+                })
+                .then(result => {
+                  console.log(result);
+
+                  thumb.push(result);
+                  console.log('This is the final Array of Objects: \n', thumb);
+                  setMedia(thumb);
+                });
+          }
+
         });
   };
 
