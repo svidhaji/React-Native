19a87fe3206f5a8aa504ed20a234ff3b5c92db62
svidhaji <svidhaji@gmail.com>
2019-09-09 10:05:28 +0300

    pp changes

--- components/views/Single.js
+++ components/views/Single.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, Text} from 'react-native';
 import PropTypes from 'prop-types';
+import AImage from '../AsyncImage';
 
 const Single = (props) => {
   const {navigation} = props;
   console.log('Singel navi', navigation.state);
   const file = navigation.state.params.file;
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>{file.title}</Text>
+      <AImage
+        source={{uri: 'http://media.mw.metropolia.fi/wbma/uploads/' + file.filename}}
+        style={{
+          borderRadius: 50,
+          width: '100%',
+          height: '90%',
+        }}
+        spinnerColor='#b3e5fc'
+      />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 Single.propTypes = {
   navigation: PropTypes.object,
